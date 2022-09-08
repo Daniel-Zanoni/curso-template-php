d6a4c7f817c3cbcf2d25f3de40572a7d4f475730
Daniel Zanoni <danielzgjob@outlook.com>
2022-09-07 22:41:35 -0300

    How output buffer work

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Home';
+$data = ['name' => 'Daniel', 'age' => 21];
+
+ob_start(); // Todo o conteúdo dentro de ob_start(), será armazenado na variável $content.
+
+extract($data);
+
+require 'home.php';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump($content);
